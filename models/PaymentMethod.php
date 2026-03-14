1f29d8a0dd36a0342a8c4cd647a25c71952840d3
Mysuperuser237 <bangzakiatou1999@gmail.com>
2026-03-14 19:54:01 +0100

    AI generate file

---
+++ models/PaymentMethod.php
@@ -0,0 +1,9 @@
+<?php
+class PaymentMethod {
+    public $id;
+    public $name;
+    public $type;
+
+    // Méthodes pour récupérer, créer, mettre à jour et supprimer des méthodes de paiement
+
+}
