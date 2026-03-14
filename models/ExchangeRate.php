17d527baa97d4b2b78632708db78e70a47592be0
Mysuperuser237 <bangzakiatou1999@gmail.com>
2026-03-14 19:54:04 +0100

    AI generate file

---
+++ models/ExchangeRate.php
@@ -0,0 +1,9 @@
+<?php
+class ExchangeRate {
+    public $currency_pair;
+    public $rate;
+    public $updated_at;
+
+    // Méthodes pour récupérer, créer, mettre à jour et supprimer des taux de change
+
+}
