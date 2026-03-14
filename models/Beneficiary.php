f3c2569c7297955f91b4554fc4a65dbbf7d34467
Mysuperuser237 <bangzakiatou1999@gmail.com>
2026-03-14 19:54:02 +0100

    AI generate file

---
+++ models/Beneficiary.php
@@ -0,0 +1,13 @@
+<?php
+class Beneficiary {
+    public $id;
+    public $user_id;
+    public $name;
+    public $account_number;
+    public $bank_name;
+    public $swift_code;
+    public $country;
+ 
+    // Méthodes pour récupérer, créer, mettre à jour et supprimer des bénéficiares
+
+}
