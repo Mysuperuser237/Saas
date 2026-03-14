586f6094ce1a89f91be5ec47dd6de8b0db54381b
Mysuperuser237 <bangzakiatou1999@gmail.com>
2026-03-14 19:54:00 +0100

    AI generate file

---
+++ models/User.php
@@ -0,0 +1,12 @@
+<?php
+class User {
+    public $id;
+    public $name;
+    public $email;
+    public $password;
+    public $company_name;
+    public $country;
+
+    // Méthodes pour récupérer, créer, mettre à jour et supprimer des utilisateurs
+
+}
