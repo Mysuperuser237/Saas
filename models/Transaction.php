46121a6e3344f51a490d87f3975c58ad22307af1
Mysuperuser237 <bangzakiatou1999@gmail.com>
2026-03-14 19:53:58 +0100

    AI generate file

---
+++ models/Transaction.php
@@ -0,0 +1,16 @@
+<?php
+class Transaction {
+    public $id;
+    public $user_id;
+    public $amount;
+    public $currency_from;
+    public $currency_to;
+    public $exchange_rate;
+    public $payment_method;
+    public $status;
+    public $created_at;
+    public $updated_at;
+
+    // Méthodes pour récupérer, créer, mettre à jour et supprimer des transactions
+
+}
